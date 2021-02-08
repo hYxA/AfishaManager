c89f276b6b8ff55e52267ef8db9556d1f1b78dbc
hYxA <zybu@live.ru>
2021-02-08 17:17:48 +0300

    update repo

--- src/main/java/manager/AfishaManager.java
+++ src/main/java/manager/AfishaManager.java
@@ -1,34 +1,26 @@
 package manager;
 
+import repository.AfishaRepository;
+
 public class AfishaManager {
-    public AfishaManager(int countFilms) {
-        int id;
-        int filmPrise;
-        String filmName;
-    }
+    // создание пустого массива
+
+
+    public AfishaManager(int countFilms) {    }
 
     AfishaManager manager = new AfishaManager(5);
 
-    // создание пустого массива
-    private FilmItem[] items = new FilmItem[0];
+    private AfishaRepository repository;
 
+    public AfishaManager(AfishaRepository repository) {this.repository = repository;}
 
+    public void add(FilmItem item) {repository.save(item);}
+
+    public void getAll() {repository.getAfisha();}
 
     public void addFilm(FilmItem item) {
-        int length = items.length + 1;
-        FilmItem[] tmp = new FilmItem[length];
-        // копируем поэлементно
-        System.arraycopy(items, 0, tmp, 0, length);
+
     }
 
-    public FilmItem[] getItems() {
-        // создание массива для хранения результатов
-        FilmItem[] result = new FilmItem[items.length];
-        // перебираем результаты
-        for (int i = 0; i < result.length; i++) {
-            int index = items.length - i - 1;
-            result[i] = items[index];
-        }
-        return result;
-    }
+
 }
--- src/main/java/repository/AfishaRepository.java
+++ src/main/java/repository/AfishaRepository.java
@@ -1,14 +1,49 @@
 package repository;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import manager.FilmItem;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
 public class AfishaRepository {
+    // создание массива для хранения результатов
+    private FilmItem[] items = new FilmItem[0];
+    FilmItem[] result = new FilmItem[items.length];
+    private int id;
+    private int filmPrise;
+    private String filmName;
 
-    //TODO
-    // findAll - возвращает массив всех хранящихся в массиве объектов
-    // save - добавляет объект в массив
-    // findById - возвращает объект по идентификатору (либо null, если такого объекта нет)
-    // removeById - удаляет объект по идентификатору (если объекта нет, то пусть будет исключение, как на лекции)
-    // removeAll* - полностью вычищает репозиторий
+    public void save(FilmItem item) {
+        int length = items.length + 1;
+        FilmItem[] tmp = new FilmItem[length];
+        // копируем поэлементно
+        System.arraycopy(items, 0, tmp, 0, length);
 
+        // кладём последним наш элемент
+        int lastIndex = tmp.length - 1;
+        tmp[lastIndex] = item;
+        items = tmp;
+    }
+
+    public FilmItem[] getAfisha() {
+        // перебираем результаты
+        for (int i = 0; i < result.length; i++) {
+            int index = items.length - i - 1;
+            result[i] = items[index];
+        }
+        return result;
+    }
+
+//TODO
+// findAll - возвращает массив всех хранящихся в массиве объектов
+// save - добавляет объект в массив
+// findById - возвращает объект по идентификатору (либо null, если такого объекта нет)
+// removeById - удаляет объект по идентификатору (если объекта нет, то пусть будет исключение, как на лекции)
+// removeAll* - полностью вычищает репозиторий
 
 
 }
