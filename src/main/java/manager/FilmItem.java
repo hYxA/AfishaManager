bc8781527d7e3642b220e19c8862f0c54c5e4c88
hYxA <zybu@live.ru>
2021-02-08 15:49:35 +0300

    create FilmItem

---
+++ src/main/java/manager/FilmItem.java
@@ -0,0 +1,15 @@
+package manager;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+public class FilmItem {
+    private int id;
+    private int filmPrise;
+    private String filmName;
+}
