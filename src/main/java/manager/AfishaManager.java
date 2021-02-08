fca8ec0fbc12ac28de7c649af207e47e10479071
hYxA <zybu@live.ru>
2021-02-08 16:00:56 +0300

    create AfishaManager

---
+++ src/main/java/manager/AfishaManager.java
@@ -0,0 +1,25 @@
+package manager;
+
+public class AfishaManager {
+
+    // создание пустого массива
+    private FilmItem[] items = new FilmItem[0];
+
+    public void addFilm(FilmItem item) {
+        int length = items.length + 1;
+        FilmItem[] tmp = new FilmItem[length];
+        // копируем поэлементно
+        System.arraycopy(items, 0, tmp, 0, length);
+    }
+
+    public FilmItem[] getItems() {
+        // создание массива для хранения результатов
+        FilmItem[] result = new FilmItem[items.length];
+        // перебираем результаты
+        for (int i = 0; i < result.length; i++) {
+            int index = items.length - i - 1;
+            result[i] = items[index];
+        }
+        return result;
+    }
+}
