e0b09c6807f7b75b1955a89df632f4f9e62dc378
hYxA <hYxA@users.noreply.github.com>
2021-04-08 15:00:46 +0300

    remove excess tests

--- src/test/java/ru/netology/manager/AfishaManagerTest.java
+++ src/test/java/ru/netology/manager/AfishaManagerTest.java
@@ -3,36 +3,23 @@ package ru.netology.manager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.domain.FilmItem;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class AfishaManagerTest {
     AfishaManager manager = new AfishaManager(5);
 
-    private final FilmItem first = new FilmItem(
-            1, 300, "first");
-    private final FilmItem second = new FilmItem(
-            2, 250, "second");
-    private final FilmItem third = new FilmItem(
-            3, 550, "third");
-    private final FilmItem four = new FilmItem(
-            4, 300, "four");
-    private final FilmItem five = new FilmItem(
-            5, 250, "five");
-    private final FilmItem six = new FilmItem(
-            6, 550, "six");
-    private final FilmItem seven = new FilmItem(
-            7, 300, "seven");
-    private final FilmItem eight = new FilmItem(
-            8, 250, "eight");
-    private final FilmItem nine = new FilmItem(
-            9, 550, "nine");
-    private final FilmItem ten = new FilmItem(
-            10, 300, "ten");
-    private final FilmItem eleven = new FilmItem(
-            11, 250, "eleven");
-    private final FilmItem twelve = new FilmItem(
-            12, 550, "twelve");
+    private final FilmItem first = new FilmItem(1, 300, "first");
+    private final FilmItem second = new FilmItem(2, 250, "second");
+    private final FilmItem third = new FilmItem(3, 550, "third");
+    private final FilmItem four = new FilmItem(4, 300, "four");
+    private final FilmItem five = new FilmItem(5, 250, "five");
+    private final FilmItem six = new FilmItem(6, 550, "six");
+    private final FilmItem seven = new FilmItem(7, 300, "seven");
+    private final FilmItem eight = new FilmItem(8, 250, "eight");
+    private final FilmItem nine = new FilmItem(9, 550, "nine");
+    private final FilmItem ten = new FilmItem(10, 300, "ten");
+    private final FilmItem eleven = new FilmItem(11, 250, "eleven");
+    private final FilmItem twelve = new FilmItem(12, 550, "twelve");
 
     @BeforeEach
     void prepareManager() {
@@ -54,10 +41,18 @@ class AfishaManagerTest {
     public void shouldSave() {
         manager.addFilm(first);
         FilmItem[] expected = new FilmItem[]{
-                five,
-                four,
-                third,
+                first,
                 second,
+                third,
+                four,
+                five,
+                six,
+                seven,
+                eight,
+                nine,
+                ten,
+                eleven,
+                twelve,
                 first
         };
         FilmItem[] actual = manager.getItems();
